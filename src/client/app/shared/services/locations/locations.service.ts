475b2ff6a8c1805cd62df5ebeb6d0251a70fcb03
Isaac Hookom <isaac.hookom@gmail.com>
2017-06-05 22:39:50 -0400

    cleanup locations svc

--- src/client/app/shared/services/locations/locations.service.ts
+++ src/client/app/shared/services/locations/locations.service.ts
@@ -5,12 +5,10 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
 import { Location } from '../../models/location';
-import { Config } from '../../config/env.config';
 
 @Injectable()
 export class LocationsService {
     private dataUrl = 'http://www.climbontheway.com/api/get.php';
-    // private dataUrl = 'api/get.php';
 
     constructor(private http: Http) { }
 
@@ -21,14 +19,13 @@ export class LocationsService {
     }
 
     private handleError (error: Response | any) {
-        // In a real world app, you might use a remote logging infrastructure
         let errMsg: string;
         if (error instanceof Response) {
-        const body = error.json() || '';
-        const err = body.error || JSON.stringify(body);
-        errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+            const body = error.json() || '';
+            const err = body.error || JSON.stringify(body);
+            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
         } else {
-        errMsg = error.message ? error.message : error.toString();
+            errMsg = error.message ? error.message : error.toString();
         }
         console.error(errMsg);
         return Observable.throw(errMsg);
