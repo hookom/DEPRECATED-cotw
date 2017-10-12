5937e73aa0a21dcfd055e0a25e21cb9b2904866f
Isaac <ihookom@callibrity.com>
2017-10-11 22:19:51 -0400

    working on tests

--- src/e2e/pageObjects/find.page.js
+++ src/e2e/pageObjects/find.page.js
@@ -1,31 +1,39 @@
 const until = protractor.ExpectedConditions;
 
-const openSocialWheel = 'div.c-5';
-const bellIcon = 'div.c-1 span a';
-const gearIcon = 'div.c-2 span a';
-const facebookIcon = 'div.c-3 span a';
-const twitterIcon = 'div.c-4 span a';
+const toggleSocialWheel = 'div.c-5 span';
+const bellIcon = 'div.c-1 span a.link';
+const gearIcon = 'div.c-2 span a.link';
+const facebookIcon = 'div.c-3 span a.link';
+const twitterIcon = 'div.c-4 span a.link';
 
 var findPage = {
     navigateTo: () => {
         browser.get('http://localhost:5555');
     },
 
-    openSocialMenu: () => {
-        var socialPlus = element(by.css(openSocialWheel));
-        browser.wait(until.presenceOf(socialPlus), 5000, 'Social menu plus element did not display')
-            .then(() => {socialPlus.click()});
+    toggleSocialMenu: () => {
+        var socialToggle = element(by.css(toggleSocialWheel));
+        return browser.wait(until.presenceOf(socialToggle), 5000, 'Social menu plus element did not display')
+            .then(() => {
+                socialToggle.click();
+            });
     },
 
     socialMenuIsLoaded: () => {
-        var bellP = browser.wait(until.presenceOf(bellIcon), 5000, 'Bell icon link element did not display');
-        var gearP = browser.wait(until.presenceOf(gearIcon), 5000, 'Gear icon link element did not display');
-        var facebookP = browser.wait(until.presenceOf(facebookIcon), 5000, 'Facebook icon link element did not display');
-        var twitterP = browser.wait(until.presenceOf(twitterIcon), 5000, 'Twitter icon link element did not display');
+        var bellP = browser.wait(until.visibilityOf($(bellIcon)), 5000, 'Bell icon link element did not display');
+        var gearP = browser.wait(until.visibilityOf($(gearIcon)), 5000, 'Gear icon link element did not display');
+        var facebookP = browser.wait(until.visibilityOf($(facebookIcon)), 5000, 'Facebook icon link element did not display');
+        var twitterP = browser.wait(until.visibilityOf($(twitterIcon)), 5000, 'Twitter icon link element did not display');
 
         return Promise.all([bellP, gearP, facebookP, twitterP])
-            .then(() => { 
-                return resolve(true);
+            .then((links) => {
+                var loaded = true;
+                links.forEach(visible => {
+                    if (!visible) {
+                        loaded = false;
+                    }
+                });
+                return loaded;
             });
     }
 }
--- src/e2e/tests/find.spec.js
+++ src/e2e/tests/find.spec.js
@@ -4,19 +4,18 @@ describe('Find Page Tests - ', function() {
 	it('should open social menu wheel', function() {
 
 		findPage.navigateTo();
-		findPage.openSocialMenu();
-		findPage.socialMenuIsLoaded()
+		findPage.toggleSocialMenu()
 			.then(() => {
-
+				findPage.socialMenuIsLoaded()
+					.then((loaded) => {
+						console.log('asserting on: ' + loaded);
+						expect(loaded).toEqual(true);
+					}, (rejectReason) => {
+						console.log('promise rejection')
+						console.log(rejectReason);
+					});
 			});
-		// browser.sleep(5000);
-		// element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-		// var todoList = element.all(by.repeater('todo in todoList.todos'));
-		// expect(todoList.count()).toEqual(3);
-		// expect(todoList.get(2).getText()).toEqual('write first protractor test');
-
-		// todoList.get(2).element(by.css('input')).click();
-		// var completedAmount = element.all(by.css('.done-true'));
-		// expect(completedAmount.count()).toEqual(2);
+		
+		browser.sleep(2000);
 	});
 });
